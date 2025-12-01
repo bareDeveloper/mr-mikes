f834f2759fffa96c17e331c35d8c814a0b457d1d
Mark Roberts <mark@reaction.ca>
2025-12-01 15:18:24 -0800

    downloaded live site files

---
+++ settings/cache-enabler/mrmikes.wpengine.com.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Cache Enabler settings for https://mrmikes.wpengine.com
+ *
+ * @since      1.5.0
+ * @change     1.5.0
+ *
+ * @generated  Fri, 09 Apr 2021 22:25:22 GMT
+ */
+
+return array (
+  'version' => '1.7.1',
+  'permalink_structure' => 'has_trailing_slash',
+  'cache_expires' => 0,
+  'cache_expiry_time' => 0,
+  'clear_site_cache_on_saved_post' => 0,
+  'clear_site_cache_on_saved_comment' => 0,
+  'clear_site_cache_on_changed_plugin' => 1,
+  'convert_image_urls_to_webp' => 1,
+  'mobile_cache' => 0,
+  'compress_cache' => 1,
+  'minify_html' => 0,
+  'minify_inline_css_js' => 0,
+  'excluded_post_ids' => '74,2529,2722,6424,6438,8583,11877,12089,23688,24078',
+  'excluded_page_paths' => '/(\\/jobs\\/.*)/',
+  'excluded_query_strings' => '',
+  'excluded_cookies' => '',
+);
