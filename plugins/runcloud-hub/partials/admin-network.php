9abcce27fb1a1697e8ec4e5f65668544cb60a2e6
Mark Roberts <mark@reaction.ca>
2025-12-01 13:55:28 -0800

    Update gitignore and untrack ignored plugins

--- plugins/runcloud-hub/partials/admin-network.php
+++
@@ -1,2 +0,0 @@
-<?php defined('RUNCLOUD_HUB_INIT') || exit;?>
-<?php self::view_page('admin');?>
--- plugins/runcloud-hub/partials/sidebar-md.php
+++
@@ -1,3 +0,0 @@
-<?php defined('RUNCLOUD_HUB_INIT') || exit;?>
-
-<?php self::view_page('sidebar');?>
